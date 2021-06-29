dc4887f2e320094592c0d31079b71ee820dd00d7
purhan <purhan01@gmail.com>
2021-06-29 15:18:05 +0530

    [feature] Implement SNMP check #297
    Closes #297

--- openwisp_monitoring/check/settings.py
+++ openwisp_monitoring/check/settings.py
@@ -6,9 +6,16 @@ CHECK_CLASSES = getattr(
     (
         ('openwisp_monitoring.check.classes.Ping', 'Ping'),
         ('openwisp_monitoring.check.classes.ConfigApplied', 'Configuration Applied'),
+        (
+            'openwisp_monitoring.check.classes.SnmpDeviceMonitoring',
+            'SNMP Device Monitoring',
+        ),
     ),
 )
 AUTO_PING = getattr(settings, 'OPENWISP_MONITORING_AUTO_PING', True)
+AUTO_SNMP_DEVICEMONITORING = getattr(
+    settings, 'OPENWISP_MONITORING_AUTO_SNMP_DEVICEMONITORING', True
+)
 AUTO_CONFIG_CHECK = getattr(
     settings, 'OPENWISP_MONITORING_AUTO_DEVICE_CONFIG_CHECK', True
 )
